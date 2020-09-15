7d6f12771b063de0733555d194152b142f749a83
中山颯 <sippu242@gmail.com>
2020-09-15 14:56:23 +0900

    [add] product list

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask_bootstrap import Bootstrap
+from flask_fontawesome import FontAwesome
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
+fa = FontAwesome(app)
 
 @app.route('/')
 def hello():
