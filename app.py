e2e1e57ad7c4a8e46980ac27dbe89ae97a2f4bed
中山颯 <sippu242@gmail.com>
2020-09-15 14:24:31 +0900

    [add] links to today's videos

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+from flask_bootstrap import Bootstrap
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
+
 @app.route('/')
 def hello():
-    return render_template('index.html') 
+    return render_template('index.html')
