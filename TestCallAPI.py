13338ebd29a1fade607d95fe3a2215f759f29802
AO YANJIAO <aoyanjiao@gmail.com>
2020-09-16 14:20:27 +0900

    add exaxt genreId

--- TestCallAPI.py
+++ TestCallAPI.py
@@ -5,8 +5,8 @@ def Item_Search_API(keyword):
     Item_Search_API_serch_param={"format":"json","keyword":keyword,"applicationId":appId}
     Item_Search_API_result = requests.get(Item_Search_API_Endpoint,Item_Search_API_serch_param).json()
     return Item_Search_API_result
-def Ranking_API(gID):
+def Ranking_API(gID):#gId of sports is 551942, gId of healthy food is 100987
     Ranking_API_Endpoint = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20170628?"
-    Ranking_API_serch_param={"applicationId":appId,"geneId":gID}
+    Ranking_API_serch_param={"applicationId":appId,"genreId":gID,"page":1}
     Ranking_API_result = requests.get(Ranking_API_Endpoint,Ranking_API_serch_param).json()
     return Ranking_API_result
