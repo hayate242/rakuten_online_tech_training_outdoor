a80dc01f8d46d725fba6ff589fd8e788250102a7
Ryo <ryotaro.www.good@gmail.com>
2020-09-16 11:05:31 +0900

    restore app.py

--- app.py
+++ app.py
@@ -7,16 +7,7 @@ app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
 fa = FontAwesome(app)
-appId=1018661814203004669
-gID=100938#genre ID of Healthcare items
-Item_Search_API_Endpoint = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?"
-Item_Search_API_serch_param={"format":"json","keyword":"","applicationId":appId}
-Item_Search_API_result = requests.get(Item_Search_API_Endpoint,Item_Search_API_serch_param).json()
-print(len(Item_Search_API_result["Items"]))
-Ranking_API_Endpoint = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20170628?"
-Ranking_API_serch_param={"applicationId":appId,"geneId":gID}
-Ranking_API_result = requests.get(Ranking_API_Endpoint,Ranking_API_serch_param).json()
-print(len(Item_Search_API_result["Items"]))
+
 
 @app.route('/')
 def index():
